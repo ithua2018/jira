55a1fa9805725d212ae9a2c43dc88b1d41130647
ithua <673509045@qq.com>
2022-05-20 08:23:32 +0800

    style: 样式修改

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,3 +1,4 @@
+import styled from "@emotion/styled";
 import { useAuth } from "context/auth-context";
 import React from "react";
 import { ProjectListScreen } from "screens/project-list";
@@ -5,7 +6,19 @@ import { ProjectListScreen } from "screens/project-list";
 export const AuthenticatedApp = () => {
     const {logout} = useAuth()
     return <div>
-        <button onClick={logout}>登出</button>
-        <ProjectListScreen/>
+        <PageHeader>
+            <button onClick={logout}>登出</button>
+        </PageHeader>
+        <Main>
+             <ProjectListScreen/>
+        </Main>
+        
     </div>
 }
+
+const  PageHeader  = styled.header`
+    height: 6rem;
+`
+const Main = styled.main`
+    height: calc(100vh - 6rem);
+`
